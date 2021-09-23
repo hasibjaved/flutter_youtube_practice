9f7939927b744061a3e0c718348c9c3031f8c31c
Hasib Javed <haseeb_javaid@live.com>
2021-09-23 12:01:56 +0500

    Rows and Columns Lesson Complete

--- lib/main.dart
+++ lib/main.dart
@@ -22,35 +22,45 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Awesome App'),
       ),
-     body:Center(
-       
+     body: Padding(
+       padding: const EdgeInsets.all(8.0),
        child: Container(
-         padding: const EdgeInsets.all(8),
-         alignment: Alignment.center,
-        //  color: Colors.teal,
-         width: 100,
-         height: 100,
-         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [BoxShadow(color: Colors.grey,
-          blurRadius: 5,
-          offset: Offset(2.0,5.0))],
-           color: Colors.teal,
-           gradient: LinearGradient(colors: [
-             Colors.yellow, Colors.pink
-           ],
-           )
-           
-           ),
-         child: Text('This is circle',
-         textAlign: TextAlign.center,
-         style: TextStyle(
-           color: Colors.black,
-           fontWeight: FontWeight.bold,
-           fontSize: 20,
-         ),),
+         color: Colors.black,
+         width: MediaQuery.of(context).size.width,
+         height: MediaQuery.of(context).size.height,
+         child: Padding(
+           padding: const EdgeInsets.all(8.0),
+           child: Column(
+               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+               crossAxisAlignment: CrossAxisAlignment.center,
+               children: <Widget>[
+                  Container(
+               padding: const EdgeInsets.all(8),
+               width: 100,
+               height: 100,
+               color: Colors.red,
+             ),
+              Container(
+               padding: const EdgeInsets.all(8),
+
+               width: 100,
+               height: 100,
+               color: Colors.green,
+             ),
+              Container(
+               padding: const EdgeInsets.all(8),
+
+               width: 100,
+               height: 100,
+               color: Colors.blue,
+             ),
+               ],
+             ),
+         ),
        ),
      )
+     
     );
   }
 }
+
