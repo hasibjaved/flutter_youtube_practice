3d6679d1e702b6da157e0e0d62ded55a3b853700
Hasib Javed <haseeb_javaid@live.com>
2021-09-24 11:24:49 +0500

    Shared Preferences contd.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
-import 'dart:js';
+// import 'dart:js';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_youtube_practice/pages/login_page.dart';
+import 'package:flutter_youtube_practice/utils/constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'pages/home_page.dart';
 
-void main()
+Future main() async
 {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Constants.prefs = await SharedPreferences.getInstance();
   runApp(MaterialApp(
     title: 'Awesome App',
     home: LoginPage(),
